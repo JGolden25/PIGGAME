1ba98a02fd0d831639a23a92feadb5b733cec9cd
Joseph Chretien-Golden <joechre@gmail.com>
2019-10-10 21:41:55 -0500

    ACTIVE PLAYERS swap error now

--- starter/app.js
+++ starter/app.js
@@ -10,9 +10,7 @@ GAME RULES:
 */
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 1;
+init()
 
 dice = Math.floor(Math.random() * 6) + 1;
 console.log(dice);
@@ -64,6 +62,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     //Check if player won the game
     if (scores[activePlayer] >= 50) {
         document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
     }
     else {
         //Next Player
@@ -85,6 +85,23 @@ function nextPlayer() {
    // document.querySelector('.player-0-panel').classList.remove('active');
      
     document.querySelector('dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init); 
+
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textcontent = '0';
+document.getElementById('score-1').textcontent = '0';
+document.getElementById('current-0').textcontent = '0';
+document.getElementById('current-1').textcontent = '0'; 
+document.querySelector('#name-' + activePlayer).textContent = 'Player 1';
+document.querySelector('#name-' + activePlayer).textContent = 'Player 2';
 }
 // document.querySelector('#current-' + activePlayer).textContent =  dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
